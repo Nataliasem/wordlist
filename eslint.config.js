fd292708dd7190653352cadff533f6e5d61d34bd
natalia_semka <seraya.93@inbox.ru>
2025-09-08 13:54:25 +0300

    update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,16 @@
 import eslint from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
+import globals from 'globals'
 
 export default [
   eslint.configs.recommended,
-  ...pluginVue.configs['flat/recommended']
+  ...pluginVue.configs['flat/recommended'],
+  {
+    languageOptions: {
+      sourceType: 'module',
+      globals: {
+        ...globals.browser
+      }
+    }
+  }
 ]
