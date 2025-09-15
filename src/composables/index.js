ed733cdd989d341e9f03bea61deb23b587a1161b
natalia_semka <seraya.93@inbox.ru>
2025-09-15 20:06:34 +0300

    Delete unused composable

--- src/composables/index.js
+++ src/composables/index.js
@@ -1,11 +1,9 @@
 import { useCustomFetch } from './useCustomFetch.js'
 import { useFormValidation } from './useFormValidation.js'
 import { useModal } from './useModal.js'
-import { useFilterBySearch } from './useFilterBySearch.js'
 
 export {
   useCustomFetch,
   useFormValidation,
-  useModal,
-  useFilterBySearch
+  useModal
 }
