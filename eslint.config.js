129fe882c4be7547a044e88dabb338d3d41b936d
natalia_semka <seraya.93@inbox.ru>
2025-09-22 16:14:18 +0300

    add typescript-eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,24 @@
-import eslint from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 import globals from 'globals'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 
-export default [
-  eslint.configs.recommended,
-  ...pluginVue.configs['flat/recommended'],
+export default defineConfigWithVueTs(
+  pluginVue.configs['flat/recommended'],
+   vueTsConfigs.recommended,
+   vueTsConfigs.stylistic,
   {
+    files: ['**/*.{js,mjs,cjs,ts,vue}'],
     languageOptions: {
+      ecmaVersion: 'latest',
       sourceType: 'module',
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+        extraFileExtensions: ['.vue'],
+      },
       globals: {
         ...globals.browser
       }
     }
   }
-]
+)
