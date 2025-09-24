3d6e6e9b1bb67db4cd4f0fba7429889de76e2f79
natalia_semka <seraya.93@inbox.ru>
2025-09-24 13:45:33 +0300

    update useFilterBySearch

--- src/composables/useFilterBySearch.js
+++ src/composables/useFilterBySearch.js
@@ -1,17 +1,19 @@
-import { ref, computed } from 'vue'
+import { computed } from 'vue'
+import { useSearch } from '@/composables/index.js'
 import { filterBySearchString } from '@/utils/index.js'
 
+const {
+  searchString,
+  clearSearch
+} = useSearch()
+
 export function useFilterBySearch(store, filteredField) {
-  const searchString = ref('')
   const filteredData = computed(() => {
     return filterBySearchString(store.data, filteredField, searchString.value)
   })
   const isFilteredDataEmpty = computed(() => {
     return filteredData.value.length === 0
   })
-  const clearSearch = () => {
-    searchString.value = ''
-  }
 
   return {
     searchString,
