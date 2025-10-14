5401798c9acb7e209ea7a15b0cba2fc8c37d2095
natalia_semka <seraya.93@inbox.ru>
2025-10-14 12:34:36 +0300

    fix useCategoryFetch.ts

--- src/composables/category/useCategoryFetch.ts
+++ src/composables/category/useCategoryFetch.ts
@@ -1,41 +1,41 @@
 import { computed } from 'vue'
 import { useCustomFetch, useSearch } from '@/composables'
-import { filterBySearchString } from '@/utils'
 import { getCategories } from '@/api/category'
 import { FETCH_CATEGORY_MESSAGE } from '@/constants'
 
 export function useCategoryFetch() {
-  const {
-    searchString,
-    hasActiveSearch,
-    clearSearch
-  } = useSearch()
+    const {
+        searchString,
+        hasActiveSearch,
+        clearSearch
+    } = useSearch()
 
-  const {
-    isEmpty,
-    hasError,
-    data: categories,
-    fetchData: fetchCategories,
-  } = useCustomFetch(getCategories)
+    const {
+        isEmpty,
+        hasError,
+        data: categories,
+        fetchData: fetchCategories,
+    } = useCustomFetch(getCategories)
 
-  const filteredData = computed(() => {
-    return filterBySearchString(categories.value, 'name', searchString.value)
-  })
-  const fetchMessage = computed(() => {
-    if (hasError.value) return FETCH_CATEGORY_MESSAGE.error
-    if (!hasActiveSearch.value && isEmpty.value) return FETCH_CATEGORY_MESSAGE.empty
-    if (hasActiveSearch.value && !filteredData.value.length) return FETCH_CATEGORY_MESSAGE.emptySearch
-    return null
-  })
+    const filteredData = computed(() => {
+        return categories.value.filter(item => item.name.toLowerCase()
+            .includes(searchString.value.toLowerCase()))
+    })
+    const fetchMessage = computed(() => {
+        if (hasError.value) return FETCH_CATEGORY_MESSAGE.error
+        if (!hasActiveSearch.value && isEmpty.value) return FETCH_CATEGORY_MESSAGE.empty
+        if (hasActiveSearch.value && !filteredData.value.length) return FETCH_CATEGORY_MESSAGE.emptySearch
+        return null
+    })
 
-  return {
-    fetchMessage,
-    searchString,
-    filteredData,
-    clearSearch,
-    isEmpty,
-    hasError,
-    categories,
-    fetchCategories
-  }
+    return {
+        fetchMessage,
+        searchString,
+        filteredData,
+        clearSearch,
+        isEmpty,
+        hasError,
+        categories,
+        fetchCategories
+    }
 }
