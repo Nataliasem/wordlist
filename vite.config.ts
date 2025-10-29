346b4425499d2e52a92656c2626f647cac0a3dbb
natalia_semka <seraya.93@inbox.ru>
2025-10-29 18:14:38 +0300

    add vite-bundle-analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
+import { analyzer } from 'vite-bundle-analyzer'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss(), analyzer({
+      analyzerPort: 8889
+  })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
