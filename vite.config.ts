c15b9bcbd2315ed0295b0fb92f4cda2177f79e50
natalia_semka <seraya.93@inbox.ru>
2026-01-02 17:58:33 +0300

    update configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import { analyzer } from 'vite-bundle-analyzer'
 import vue from '@vitejs/plugin-vue'
@@ -6,23 +7,52 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [vue(), tailwindcss(), analyzer({
-        analyzerPort: 8889
-    })],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src')
-        }
-    },
-    test: {
-        globals: true,
-        environment: 'jsdom',
-        coverage: {
-            all: true,
-            provider: 'v8',
-            reporter: ['text'],
-            include: ['src/**/*.vue', 'src/**/*.ts', 'src/**/*.js'],
-            exclude: ['node_modules/', 'dist/'],
-        },
+  plugins: [vue(), tailwindcss(), analyzer({
+    analyzerPort: 8889
+  })],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
     }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    // setupFiles: ['./src/setup-tests.ts'],
+    include: ['src/**/*.{test,spec}.{js,ts,vue}'],
+    exclude: [
+      'node_modules',
+      'dist',
+      '.idea',
+      '.git',
+      '.cache',
+      '**/node_modules/**',
+      '**/dist/**'
+    ],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov', 'html'],
+      reportsDirectory: './coverage',
+      include: ['src/**/*.vue', 'src/**/*.ts', 'src/**/*.js'],
+      exclude: [
+        'src/main.ts',
+        'src/constants.ts',
+        'src/router/**',
+        'src/vite-env.d.ts',
+        'src/**/index.ts',
+        'src/constants/**',
+        'src/types/**',
+        'src/components/**/index.vue',
+        'src/composables/**/index.ts',
+        'src/plugins/**',
+        'node_modules/',
+        'dist/',
+        '**/*.d.ts',
+      ],
+    },
+    css: true,
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
