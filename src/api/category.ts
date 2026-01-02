d3584ca31e55cd91aa369d125f637f58d6cc3c15
natalia_semka <seraya.93@inbox.ru>
2026-01-02 21:44:06 +0300

    add api tests

--- src/api/category.ts
+++ src/api/category.ts
@@ -2,46 +2,46 @@ import { CATEGORY_URL, WITHOUT_CATEGORY_NAME } from '@/constants'
 import { Category } from '@/types'
 
 export const create = async (categoryName: string): Promise<Category> => {
-    const response = await fetch(`${CATEGORY_URL}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({category: categoryName})
-    })
+  const response = await fetch(`${CATEGORY_URL}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({category: categoryName})
+  })
 
-    if (response.ok) {
-        return await response.json()
-    } else {
-        throw new Error('Unable to create a category')
-    }
+  if (response.ok) {
+    return await response.json()
+  } else {
+    throw new Error('Unable to create a category')
+  }
 }
 
 export const getCategories = async (categoryName?: string): Promise<Category[]> => {
-    const response = await fetch(`${CATEGORY_URL}?categoryName=${categoryName}`)
-    if (response.ok) {
-        const res = await response.json()
-        return [{
-            id: null,
-            name: WITHOUT_CATEGORY_NAME,
-        }].concat(res.sort())
-    } else {
-        return []
-    }
+  const response = await fetch(`${CATEGORY_URL}?categoryName=${categoryName}`)
+  if (response.ok) {
+    const res = await response.json()
+    return [{
+      id: null,
+      name: WITHOUT_CATEGORY_NAME,
+    }].concat(res)
+  } else {
+    return []
+  }
 }
 
 export const update = async (category: Category): Promise<void> => {
-    await fetch(`${CATEGORY_URL}/${category.id}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({category: category.name})
-    })
+  await fetch(`${CATEGORY_URL}/${category.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({category: category.name})
+  })
 }
 
 export const remove = async (id: number): Promise<void> => {
-    await fetch(`${CATEGORY_URL}/${id}`, {
-        method: 'DELETE'
-    })
+  await fetch(`${CATEGORY_URL}/${id}`, {
+    method: 'DELETE'
+  })
 }
