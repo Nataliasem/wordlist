5e54d01789ab276b7205341fdd756c7eb86a69a5
natalia_semka <seraya.93@inbox.ru>
2026-01-08 20:15:55 +0300

    Fix useExpanded, add tests

--- src/composables/useExpanded.ts
+++ src/composables/useExpanded.ts
@@ -1,11 +1,12 @@
 import { useWindowSize } from '@vueuse/core'
 import { ref } from 'vue'
 
-const isExpanded = ref(true)
+const TABLET_SCREEN_SIZE = 1024
 
 export function useExpanded() {
   const { width } = useWindowSize()
-  const TABLET_SCREEN_SIZE = 1024
+
+  const isExpanded = ref(true)
   if (width.value < TABLET_SCREEN_SIZE) {
     isExpanded.value = false
   }
